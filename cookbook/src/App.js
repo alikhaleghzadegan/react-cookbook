f77fe359522267f327d81e30f499efedf59a368f
chinch42 <vanessakolund@gmail.com>
2022-07-06 09:30:45 +0200

    React app

---
+++ cookbook/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
