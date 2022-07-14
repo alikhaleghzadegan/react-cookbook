605eb949fb55a66065f7bdad754ff44ff37997b8
anoj-kt <anojk28@gmail.com>
2022-07-14 09:27:37 +0200

    Converting DisplayRecipe comp to Bootstrap

--- src/DisplayRecipe.js
+++ src/DisplayRecipe.js
@@ -1,3 +1,7 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { useParams } from "react-router-dom";
 
 const DisplayRecipe = ({ recipes }) => {
@@ -11,19 +15,52 @@ const DisplayRecipe = ({ recipes }) => {
         if (element.id == recipeId) {
             
             return (
-                <div key={element.id}>
-                    <h2 >{element.title}</h2>
-                    <h3>{element.shortDescription}</h3>
-                    <h2>Ingredients</h2>
-                    <ul>
-                        {element.ingredients.map((eachIngredient) => {
-                            return <li>{eachIngredient}</li>
-                        })}
-                    </ul>
-                    <img src={element.image.fields.file.url} />
-                    <h2>Description</h2>
-                    <p>{element.longDescription}</p>
-                </div>
+                <Container >
+                    <Row>
+                        <Col>
+                            <h2 >{element.title}</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h3>{element.shortDescription}</h3>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h2>Ingredients</h2>
+                            <ul>
+                                {element.ingredients.map((eachIngredient) => {
+                                    return <li>{eachIngredient}</li>
+                                })}
+                            </ul>
+                        </Col>
+                        <Col>
+                            <img src={element.image.fields.file.url} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h2>Description</h2>
+                            <p>{element.longDescription}</p>
+                        </Col>
+                    </Row>
+
+                    {/* <div key={element.id}>
+                        <h2 >{element.title}</h2>
+                        <h3>{element.shortDescription}</h3>
+                        <h2>Ingredients</h2>
+                        <ul>
+                            {element.ingredients.map((eachIngredient) => {
+                                return <li>{eachIngredient}</li>
+                            })}
+                        </ul>
+                        <img src={element.image.fields.file.url} />
+                        <h2>Description</h2>
+                        <p>{element.longDescription}</p>
+                    </div> */}
+                </Container >
+                
             )
         }})
 }
